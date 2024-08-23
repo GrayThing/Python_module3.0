e25362c878c278e8b7e93dcb9cb23bcfac0e25c9
GrayThing <samorodov-artem@mail.ru>
2024-08-23 16:25:56 +0300

    Выполнено практическое задание 2023/10/12 00:00|Дополнительное практическое задание по модулю*

---
+++ module_3_hard.py
@@ -0,0 +1,31 @@
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def calculate_structure_sum(*args):
+    res = 0
+    for value in args:
+        if isinstance(value, int):
+            res += value
+        if isinstance(value, str):
+            res += len(value)
+        if isinstance(value, list):
+            res += calculate_structure_sum(*value)
+        if isinstance(value, dict):
+            values_in_dict = list(value.items())
+            res += calculate_structure_sum(*values_in_dict)
+        if isinstance(value, tuple):
+            res += calculate_structure_sum(*value)
+        if isinstance(value, set):
+            res += calculate_structure_sum(*value)
+    return res
+
+
+print(calculate_structure_sum(data_structure))
+
+
